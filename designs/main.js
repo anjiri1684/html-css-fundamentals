32243736a9bb66749055c812fff90847fc18802d
vincent anjiri <vincentanjiri12@gmail.com>
2024-07-13 23:30:01 +0300

    project complete

--- designs/main.js
+++ designs/main.js
@@ -1,1 +1,12 @@
-document.getElementById("date1").innerHTML = new Date().getFullYear();
+let currency = prompt("Which currency would you like to convert");
+if (currency === "dollar") {
+  let amount = Number(prompt("Enter amount"));
+  alert(amount * 130);
+} else if (currency === "euro") {
+  let amount = Number(prompt("Enter amount"));
+  alert(amount * 230);
+} else {
+  alert(
+    "Our system at the moment only suppot dollar to ksh and euro to cash..keep on checking to other currencies converter in future"
+  );
+}
