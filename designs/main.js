fd8cd19d58b69246a14801a9ca9c433fa685426b
vincent anjiri <vincentanjiri12@gmail.com>
2024-07-14 11:47:33 +0300

    first commit

--- designs/main.js
+++ designs/main.js
@@ -1,12 +1,2 @@
-let currency = prompt("Which currency would you like to convert");
-if (currency === "dollar") {
-  let amount = Number(prompt("Enter amount"));
-  alert(amount * 130);
-} else if (currency === "euro") {
-  let amount = Number(prompt("Enter amount"));
-  alert(amount * 230);
-} else {
-  alert(
-    "Our system at the moment only suppot dollar to ksh and euro to cash..keep on checking to other currencies converter in future"
-  );
-}
+const date1 = document.getElementById("date1");
+date1.innerHTML = new Date().getFullYear();
